dec69b0e8137c866e909e1ac83e6acb3e5a12e54
Subodh Salve <107650129+subodhsalve14@users.noreply.github.com>
2023-07-26 15:50:20 +0000

    Commit Changes

--- Basics/06_nums_and_maths.js
+++ Basics/06_nums_and_maths.js
@@ -12,7 +12,7 @@ const otherNumber = 123.8966
 // console.log(otherNumber.toPrecision(4));
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString('en-IN'));
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
