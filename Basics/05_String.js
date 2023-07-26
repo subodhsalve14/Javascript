ced47c5fb943a0e7e5a5a499c04ea880a943d088
Subodh Salve <107650129+subodhsalve14@users.noreply.github.com>
2023-07-26 12:57:49 +0000

    Commit Changes

--- Basics/05_String.js
+++ Basics/05_String.js
@@ -1,9 +1,9 @@
-const name = "hitesh"
-const repoCount = 50
+const name = "subodh"
+const age = 50
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my age is ${age}`);
 
 const gameName = new String('hitesh-hc-com')
 
@@ -11,20 +11,23 @@ const gameName = new String('hitesh-hc-com')
 // console.log(gameName.__proto__);
 
 
-// console.log(gameName.length);
+// console.log(gameName.length); 13
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+// console.log(gameName.charAt(2)); //t
+// console.log(gameName.indexOf('t')); 2
 
 const newString = gameName.substring(0, 4)
 console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(1, 4)
 console.log(anotherString);
 
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant']; //Slice Example of Minus
+console.log(animals.slice(2, -1));
+
 const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
